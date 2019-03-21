10b328f3f7bdd015c71be3b9322337d5adbe8338
s1 <s1@localhost.localdomain>
2019-03-21 06:30:54 -0400

    approach changed

--- src/com/my/training/MySparkPrj.scala
+++ src/com/my/training/MySparkPrj.scala
@@ -8,9 +8,10 @@ object MySparkPrj{
   //val a = new SparkConf().setAppName("Demo").setMaster("local").set("spark.testing.memory","471859200")
   val a = new SparkConf().setAppName("MySparkPrj").setMaster("local").set("spark.testing.memory","471859200")
   val sc = new SparkContext(a)
-  val b = sc.textFile("file:///home/s1/Desktop/Data/testdata.txt")
+  //val b = sc.textFile("file:///home/s1/Desktop/Data/testdata.txt")
+  val b = sc.parallelize(List("My name is foo bar abc xyz foo bar xyz"))
   val c = b.flatMap(x=>x.split(" "))
   val d = c.map(x=>(x,1))
-  d.reduceByKey(_+_).collect()
+  d.reduceByKey(_+_).collect().foreach(println)
 }
 }
